fd8c559504f2c452c6071f3349c2ba8251ab3c5d
Ilham1m4man <ilham141623@gmail.com>
2025-07-20 12:15:17 +0700

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router";
 import "./index.css";
 import router from "./router/router.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/next"
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,6 +20,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Analytics />
     </QueryClientProvider>
   </StrictMode>
 );
