3fd4554f982e62bd8744a353cc15227be74405d2
Ilham1m4man <ilham141623@gmail.com>
2025-07-20 12:20:50 +0700

    fix prod

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import { RouterProvider } from "react-router";
 import "./index.css";
 import router from "./router/router.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Analytics } from "@vercel/analytics/next"
+import { inject } from '@vercel/analytics';
 
+inject(); 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -20,7 +21,6 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <Analytics />
     </QueryClientProvider>
   </StrictMode>
 );
